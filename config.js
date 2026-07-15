49958e09d8cd1b2df3b803f6ccc7be4c0c7a39a5
naappe <naappe@gmail.com>
2026-07-15 17:19:29 +0500

    Delete config.js; keep only index.html

--- config.js
+++
@@ -1,7 +0,0 @@
-window.CONFIG = {
-      BASE_URL: 'https://api.baserow.io',
-      TABLE_ID: '778899',
-      API_TOKEN: 'iGt3x8dgQt82m46ENIhxJB0189dZU6fe',
-      LOGIN_USER: 'admin',
-      LOGIN_PASS: 'White@5654'
-    };
