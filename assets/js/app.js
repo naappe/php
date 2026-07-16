7558c8fc9cc55a783648ff1927b67e65663c3f86
naappe <naappe@gmail.com>
2026-07-16 14:52:47 +0500

    Encode PDF-backed question-word grammar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-import {TranslationBrain,validateDhivehi,hasArabicScript,hasThaana} from './engine.js?v=3.4.2';
-import {KNOWLEDGE_VERSION,LESSON_REGISTRY,TRANSLATION_PIPELINE,VERIFIED_PAIRS,VERIFIED_WORDS,GRAMMAR_RULES} from './knowledge-base.js?v=3.4.2';
+import {TranslationBrain,validateDhivehi,hasArabicScript,hasThaana} from './engine.js?v=3.5.0';
+import {KNOWLEDGE_VERSION,LESSON_REGISTRY,TRANSLATION_PIPELINE,VERIFIED_PAIRS,VERIFIED_WORDS,GRAMMAR_RULES} from './knowledge-base.js?v=3.5.0';
 
 const $=id=>document.getElementById(id);
 let direction='en-dv';
